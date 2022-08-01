7a0df946cb2e33b253cf359258d196a544c38649
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-08-02 01:23:41 +0530

    welcome to servlets

---
+++ S01ServletBasics/src/main/java/com/servlets/example/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package com.servlets.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloWorldServlet extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter pw =res.getWriter();
+		pw.println("welcome to servlets");
+		
+	}
+
+}
