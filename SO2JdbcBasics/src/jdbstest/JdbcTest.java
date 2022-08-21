2edd9d9033382de618b564cae9b66c9a2dc63791
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-08-22 00:43:14 +0530

    changes

--- SO2JdbcBasics/src/jdbstest/JdbcTest.java
+++ SO2JdbcBasics/src/jdbstest/JdbcTest.java
@@ -7,15 +7,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcTest {
-
 	public static void main(String[] args) {
 		Connection connection = null;
 		Statement statement = null;
 		try {
-			
-		Connection connection1 = DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","nishita@123");
-		Statement statement1 = (Statement) connection1.createStatement();
-		ResultSet resultSet = ((java.sql.Statement) statement1).executeQuery("select *from account");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","nishita@123");
+		Statement statement = (Statement) connection.createStatement();
+		ResultSet resultSet = ((java.sql.Statement) statement).executeQuery("select * from account");
 		while(resultSet.next()) {
 			   System.out.println(resultSet.getInt(1) + " , " + resultSet.getString(2) + " , " + resultSet.getString(3) + " , " +resultSet.getInt(4));
 		   }
@@ -23,7 +21,7 @@ public class JdbcTest {
 			e.printStackTrace();
 		}finally {
 			try {
-				statement.close();
+				((Connection) statement).close();
 				connection.close();
 			}catch(SQLException e) {
 				e.printStackTrace();
